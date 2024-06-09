978a768ff81dd1bbb2fa2625bce68257ad2656db
Ivan Anokhin <ivanstalker@bk.ru>
2024-06-09 13:56:19 +0300

    add docstrings and coments

--- src/func.py
+++ src/func.py
@@ -2,12 +2,14 @@ import json
 
 
 def get_data():
+    """Читает и возвращает список данных из файла operations.json."""
     with open('../operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
 
 def sort_data(data):
+    """Возвращает отсортированный список по полю state=EXECUTED и по дате."""
     alt_data = []
     for i in data:
         if i.get('state') == 'EXECUTED':
@@ -17,11 +19,13 @@ def sort_data(data):
 
 
 def transform_date(data):
+    """Возвращает дату в нужном формате."""
     date = '.'.join(data['date'].split('T')[0].split('-')[::-1])
     return date
 
 
 def mask_data(bank_number):
+    """Маскирует и озвращает номера счетов и карт."""
     if bank_number is None:
         return ""
     elif len(bank_number) == 25:
@@ -30,6 +34,8 @@ def mask_data(bank_number):
 
 
 def print_data(data, date, from_, to_):
+    """"Возвращает данные об операции в нужном формате для печати."""
     return (f"{date} {data.get('description')}\n"
-          f"{from_} => {to_}\n"
-          f"{data.get('operationAmount')['amount']} {data.get('operationAmount')['currency']['name']}\n")
+            f"{from_} => {to_}\n"
+            f"{data.get('operationAmount')['amount']} "
+            f"{data.get('operationAmount')['currency']['name']}\n")
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from func import get_data, sort_data, transform_date, mask_data, print_data
 
 get_data = get_data()
 sorted_data = sort_data(get_data)
-for i in sorted_data[:5]:
+for i in sorted_data[:5]: # цикл задает количество последних операций для вывода
     date = transform_date(i)
     from_ = mask_data(i.get('from'))
     to_ = mask_data(i.get('to'))
